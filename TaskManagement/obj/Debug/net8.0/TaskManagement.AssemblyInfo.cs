8158b4c234a2f7dbc4d9949921f1d5036e223623
Shazma Afzal <shazmaafzal@gmail.com>
2025-12-17 14:12:34 +0400

    add TaskItem Model

--- TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
+++ TaskManagement/obj/Debug/net8.0/TaskManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bf7f89b8037f51b946d1f045be9fac3756f48d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
